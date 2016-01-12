0cc0fb035309a0f97f5bc5715f46dae0c27b0323
nairbspace <nairbspace@users.noreply.github.com>
2016-01-11 16:01:53 -0800

    Added define conditions for Arduino models. Added features for different GPS frequencies. Cleaned up code.

--- RacingDatalogger.ino
+++ RacingDatalogger.ino
@@ -1,188 +1,213 @@
+// Libraries
 #include <TinyGPS.h>
-#include <SoftwareSerial.h>
 #include <SPI.h>
 #include <SD.h>
 
-// Flag to turn on/off serial debugging
-// If DEBUG is set to false it will turn off all Serial statements
-// If using Arduino with multiple Serial change accordingly (Serial1, Serial2, etc.)
-// #include <Arduino.h> if necessary
-#define DEBUG true 
-#define Serial if(DEBUG)Serial 
+//Debug Condition
+#if 1 // Need this statement or else rest of if else debug conditions won't work. Search Google for explanation.
+__asm volatile("nop");
+#endif
 
+#define DUEMILANOVE true // Set to true if using Duemilanove
+#define DEBUG false  // Set to true if want to debug
+#define DEBUG_Serial if(DEBUG)Serial // If DEBUG is set to true then all DEBUG_Serial statements will work
+#if DUEMILANOVE 
+  #if DEBUG //If debugging on Duemilanove set GPS to 3,2 and use SoftwareSerial
+    #include <SoftwareSerial.h>
+    #define RX_GPS_PIN 3
+    #define TX_GPS_PIN 2
+    SoftwareSerial GPS_Serial(3,2);
+  #else //If not debugging on Duemilanove put GPS pins on 0,1
+    #define RX_GPS_PIN 0
+    #define TX_GPS_PIN 1
+    #define GPS_Serial Serial
+  #endif
+#else // If using Mega 2560 set GPS pins to Serial1
+  #define RX_GPS_PIN 19
+  #define TX_GPS_PIN 20
+  #define GPS_Serial Serial1
+#endif
+
+// Digital Pins
+// Pins 0-3 reserved for GPS on Duemilanove depending on degugging condition
+#define DEBUG_LED_PIN 4 // Debug on dash 
+#define BRAKE_PIN 5 // +12v Input. Need Optocoupler to reduce to +5V input
+#define SD_SWITCH_PIN 6 // Switch to close SD card
+#define SD_PIN 10 // SD SS Pin
+#define MOSI_PIN 11 // SD MOSI Pin
+#define MISO_PIN 12 // SD MISO Pin
+#define SCK_PIN 13 // SD SCK Pin
+
+// Analog Pins
+#define TPS_PIN A0 // Throttle Position Sensor
+
+// Initialize TinyGPS Variables
 TinyGPS gps;
+//float fmph;
+int year; 
+long lat, lon;
+byte month, day, hour, minute, second, hundredths, mph;
 
-//digital pins
-//#define rxGPSPin 0 // Receiving data from GPS
-//#define txGPSPin 1 // Transmitting data to GPS
-#define brakePin 5 // 12v Brake Light input
-SoftwareSerial softSerial(3, 2); // (rx on arduino , tx on arduino)
-#define errorPin 4 // Light for when sd card fails to initialize
-#define sdPin 10 // SD initialize pin
-//#define mosiPin 11 // MOSI pin for sd card
-//#define misoPin 12 // MISO pin for sd card
-//#define sckPin 13 // SCK pin for sd card, takes up built in LED pin
-
-//analog pins
-#define tpsPin 0 // Throttle Position Sensor pin
-
-String dataString = "";
-String gpsString = "";
-String motoString = "";
+// Initialize SD File Variable
 File dataFile;
 
-// put your setup code here, to run once:
+// Initialize Global Variables
+int brakeValue, tpsValue;
+char dataArray[60]; // Array for writing line of data receieved from GPS, Brake, TPS, etc. to SD Card
+
 void setup() {
 
-  //initialize input pins
-  pinMode(brakePin, INPUT);
-  pinMode(tpsPin, INPUT);
+  // Intitialize input pins
+  pinMode(BRAKE_PIN, INPUT);
+  pinMode(TPS_PIN, INPUT);
+  pinMode(SD_SWITCH_PIN, INPUT_PULLUP);
 
-  //initialize output pins
-  pinMode(errorPin, OUTPUT);
-  pinMode(sdPin, OUTPUT);
+  // Initialize output pins
+  pinMode(DEBUG_LED_PIN, OUTPUT);
+  pinMode(SD_PIN, OUTPUT);
 
-  digitalWrite(errorPin, HIGH); // Will turn off in any sd card issues or could not create file
+  // Initialize HIGH pins
+  digitalWrite(DEBUG_LED_PIN, HIGH);
 
-  Serial.begin(115200);
-
+  SERIAL_Initialize();
   SD_Check();
+}
+
+void loop() {
+  while(GPS_Serial.available()) {
+    if(digitalRead(SD_SWITCH_PIN) == LOW) { //If 
+      SD_Close();
+    }
+    if(gps.encode(GPS_Serial.read())) {
+      GPS_Get();
+      MOTO_Get();
+      SD_Write();     
+    }
+  }
+}
+
+void SERIAL_Initialize() {
+  // Initialize GPS and Debugging
+  // iTeadStudio GPS default set at 38400 @ 1 Hz 
+  // Go to uBlox uCenter > Edit > Messages > NMEA > PUBX to change Baud Rate
+    // Go to uBlox uCenter > Edit > Messages > UBX > CFG > Rate to change Measurement Period.
+    // Best results at 38400 bps @ 5 Hz on Arduino Hardware Serial.
+    // If using SoftwareSerial MUST use 4800 bps @ 1 Hz.
+  char baudRate[] = {0x24, 0x50, 0x55, 0x42, 0x58, 0x2C, 0x34, 0x31, 0x2C, 0x31, 0x2C, 0x30,
+            0x30, 0x30, 0x37, 0x2C, 0x30, 0x30, 0x30, 0x33, 0x2C, 0x34, 0x38, 0x30,
+            0x30, 0x2C, 0x30, 0x2A, 0x31, 0x33, 0x0D, 0x0A}; // Hex to change to 4800 bps
+  char fiveHz[] = {0xB5, 0x62, 0x06, 0x08, 0x06, 0x00, 0xC8, 0x00, 0x01, 0x00, 0x01, 0x00, 0xDE, 0x6A}; //Hex to change to 5Hz
+  char tenHz[] = {0xB5, 0x62, 0x06, 0x08, 0x06, 0x00, 0x64, 0x00, 0x01, 0x00, 0x01, 0x00, 0x7A, 0x12}; // Hex to change to 10Hz
   
-  //Header format for file to use for Text to Columns on Excel
-  String header = "MM/DD/YYYY,HH:MM:SS.CC,Delay (ms),Latitude,Longitude,Altitude (ft),MPH,TPS,Brake";
-  dataFile.println(header);
-  Serial.println(header);
-
-  // iTeadStudio GPS default set at 38400 @ 1 Hz. Go to uBlox uCenter > Edit > Messages > NMEA > PUBX
-  // to get hex or string in order to change.
-  // Also go to Edit > Messages > UBX > CFG > Rate to change Measurement Period.
-  // Best results at 38400 bps @ 5 Hz on Arduino Hardware Serial.
-  // If using SoftwareSerial must use 4800 bps @ 1 Hz.
-  softSerial.begin(38400);
-  softSerial.print("$PUBX,41,1,0007,0003,4800,0*13\r\n");
-  //Serial.write("0xB5, 0x62, 0x06, 0x08, 0x06, 0x00, 0xC8, 0x00, 0x01, 0x00, 0x01, 0x00, 0xDE, 0x6A"); //change measurement period to 5 Hz
-  softSerial.begin(4800);  
-
+  #if DUEMILANOVE
+    #if DEBUG
+      Serial.begin(115200);
+      GPS_Serial.begin(38400); 
+      delay(1000);
+      GPS_Serial.write(baudRate,sizeof(baudRate));
+      GPS_Serial.begin(4800); // Set SoftwareSerial to 4800
+    #else 
+      GPS_Serial.begin(38400);
+      delay(5000);
+      GPS_Serial.write(tenHz,sizeof(tenHz));
+      GPS_Serial.flush();
+    #endif
+  #else
+    Serial.begin(115200);
+    GPS_Serial.begin(38400);
+    delay(5000);
+    GPS_Serial.write(tenHz,sizeof(tenHz));
+        GPS_Serial.flush();
+  #endif  
 }
 
-// put your main code here, to run repeatedly:
-void loop() {  
-
-  GPS_Parser();
-
-  // If gpsString isn't blank then save to sdcard
-  if (gpsString != "") {
-    Moto_Parser();
-    dataString += gpsString;
-    dataString += ",";
-    dataString += motoString;
-    dataFile.println(dataString);
-    Serial.println(dataString);
-    
-    // Reset strings after saving to sdcard
-    gpsString = "";
-    motoString = "";
-    dataString = "";
-
-    // Save file without closing
-    dataFile.flush();
-
+void SD_Check() {
+  DEBUG_Serial.print("Initializing SD card... ");
+  if(!SD.begin(SD_PIN)) {
+    DEBUG_Serial.println("Card failed, or not present.");
+    LED_FLASH(1);
   }
-}
+  DEBUG_Serial.println("Card initialized.");
 
-// Read TinyGPS documentation and look at example sketches
-void GPS_Parser() {
-  float flat, flon, falt, fmph;
-  unsigned long fix_age;
-  int year;
-  byte month, day, hour, minute, second, hundredths;
-
-  while (softSerial.available())  {
-    if (gps.encode(softSerial.read()))  {
-
-      digitalWrite(errorPin, LOW); // turn off error pin
-
-      gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths);
-      gps.f_get_position(&flat, &flon, &fix_age);
-      falt = gps.f_altitude() * 3.28084; //convert to feet
-      fmph = gps.f_speed_mph();
-
-      char dateTime[25];
-      sprintf(dateTime, "%02d/%02d/%02d,%02d:%02d:%02d.%02d,", month, day, year, hour, minute, second, hundredths);
-      gpsString += String(dateTime);
-
-      gpsString += String(fix_age);
-
-      gpsString += String(",");
-
-      gpsString += String(flat, 6);
-      gpsString += String(",");
-      gpsString += String(flon, 6);
-      gpsString += String(",");
-      gpsString += String(falt, 2);
-      gpsString += String(",");
-      gpsString += String(fmph, 2);
-
-    }
-  } 
-}
-
-void Moto_Parser()  {
-  // Must contain .0 to calculate as float
-  float tpsPercent = 100 * (analogRead(tpsPin) / 1023.0); 
-
-  byte brakePercent;
-  if (digitalRead(brakePin) == HIGH) {
-    brakePercent = 100;
-  } else {
-    brakePercent = 0;
-  }
-
-  motoString = String(tpsPercent, 0) + "," + String(brakePercent);
-}
-
-void SD_Check () {
-
-  Serial.print("Initializing SD card... ");
-
-  if(!SD.begin(sdPin))  {
-    Serial.println("Card failed, or not present.");
-    ERROR_Light(1000);
-  }  
-
-  Serial.println("card initialized.");
-
-  // Create a new file using character array. 
-  // Must use '0' because if int i = 1, then i/10 = 0
-  // and 0 + 0 = 'null' character
-  // 0 + '0' = 0 + 48 = '0' character
-  // Reference ASCII chart for dec to char value
-  char filename[] = "LOGGER00.CSV"; // This creates array with 12 elements
-  for (byte i = 0; i < 100; i++) {
-    filename[6] = i/10 + '0'; // but 7th element is index 6
-    filename[7] = i%10 + '0';
-    if (!SD.exists(filename)) {
-      // only open a new file if it doesn't exist
-      dataFile = SD.open(filename, FILE_WRITE); 
-      break;  // leave the loop!
+  boolean fileCreate = false;
+  while(fileCreate == false) {
+    if(GPS_Serial.available() && gps.encode(GPS_Serial.read())) {
+      // Create a new file using character array. 
+      // Must use '0' because if int i = 1, then i/10 = 0
+      // and 0 + 0 = 'null' character
+      // 0 + '0' = 0 + 48 = '0' character
+      // Reference ASCII chart for dec to char value
+      char filename[] = "LOGGER00.CSV"; // This creates array with 12 elements
+      for (byte i = 0; i < 100; i++) {
+        filename[6] = i/10 + '0'; // but 7th element is index 6
+        filename[7] = i%10 + '0';
+        if (!SD.exists(filename)) {
+          // only open a new file if it doesn't exist
+          dataFile = SD.open(filename, FILE_WRITE); 
+          DEBUG_Serial.print("File ");
+          DEBUG_Serial.print(filename);
+          DEBUG_Serial.println(" has been created.");
+          fileCreate = true;
+          break;  // leave the loop!
+        }
+      }
     }
   }
-
   if (!dataFile){
-    Serial.println("could not create file.");
-    ERROR_Light(100);
+    DEBUG_Serial.println("could not create file.");
+    LED_FLASH(2);
+  }
+  String header = "MM/DD/YYYY,HH:MM:SS.CC,Latitude,Longitude,MPH,TPS,Brake";
+  dataFile.println(header);
+  DEBUG_Serial.println(header);   
+}
+
+void LED_FLASH(byte flash) {
+  // Number of times LED flashes
+  // flash = 1, SD Card failed or not present
+  // flash = 2, Could not create file
+  // flash = 3, SD card closed able to remove
+  while(1)  {
+    for (byte i = flash; i > 0; i--) {
+      digitalWrite(DEBUG_LED_PIN, LOW);
+      delay(200);
+      digitalWrite(DEBUG_LED_PIN, HIGH);
+      delay(200);
+    }
+    digitalWrite(DEBUG_LED_PIN, LOW);
+    delay(2000);
+    DEBUG_Serial.println("LED Light Blinking");
   }
 }
 
+void GPS_Get()  {
 
-// Program will end if goes into this function
-// and loop LED light
-void ERROR_Light(int ms)  {
-  while(1) {
-    digitalWrite(errorPin, LOW);
-    delay(ms);
-    digitalWrite(errorPin, HIGH);
-    delay(ms);
-    Serial.println("LED Light Blinking");
-  }
+  digitalWrite(DEBUG_LED_PIN, LOW);
+
+  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths);
+  gps.get_position(&lat, &lon);
+  //fmph = gps.f_speed_mph();
+  mph = byte(gps.f_speed_mph());
+}
+
+void MOTO_Get() {
+  tpsValue = analogRead(TPS_PIN);
+  tpsValue = map(tpsValue, 135, 793, 0 , 100);
+
+  brakeValue = digitalRead(BRAKE_PIN);
+  brakeValue = map(brakeValue, 0, 1, 0, 100);
+}
+
+void SD_Write(){
+  sprintf(dataArray, "%02d/%02d/%d,%02d:%02d:%02d.%02d,%ld,%ld,%d,%d,%d", 
+          month, day, year, hour, minute, second, hundredths, 
+          lat, lon, mph, tpsValue, brakeValue);
+  dataFile.println(dataArray);
+  DEBUG_Serial.println(dataArray);
+  dataFile.flush();
+}
+
+void SD_Close() {
+  dataFile.close();
+  LED_FLASH(3);
 }
